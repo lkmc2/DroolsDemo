167872488da193166c42186a639673a1695d886a
lkmc2 <lkmc2@163.com>
2019-08-17 10:28:39 +0800

    添加使用 Properties 为 drools 设置环境参数的例子

--- src/main/java/com/lin/ch01/KnowledgeBaseConfigurationExample1.java
+++ src/main/java/com/lin/ch01/KnowledgeBaseConfigurationExample1.java
@@ -18,6 +18,25 @@ public class KnowledgeBaseConfigurationExample1 {
 
         // 将环境配置传给知识（knowledge）定义的知识库对象
         InternalKnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase(kbConf);
+
+        /*
+        用来设置默认规则运行环境文件 drools.default.rulebase.conf 里面所涉及到的具体项内容：
+        drools.maintainTms = <true|false>
+        drools.assertBehaviour = <identity|equality>
+        drools.logicalOverride = <discard|preserve>
+        drools.sequential = <true|false>
+        drools.sequential.agenda = <sequential|dynamic>
+        drools.removeIdentities = <true|false>
+        drools.shareAlphaNodes = <true|false>
+        drools.shareBetaNodes = <true|false>
+        drools.alphaNodeHashingThreshold = <1...n>
+        drools.compositeKeyDepth = <1..3>
+        drools.indexLeftBetaMemory = <true/false>
+        drools.indexRightBetaMemory = <true/false>
+        drools.consequenceExceptionHandler = <qualified class name>
+        drools.maxThreads = <-1|1..n>
+        drools.multithreadEvaluation = <true|false>
+         */
     }
 
 }
---
+++ src/main/java/com/lin/ch01/PropertiesAsConfiguration.java
@@ -0,0 +1,28 @@
+package com.lin.ch01;
+
+import org.drools.core.impl.InternalKnowledgeBase;
+import org.drools.core.impl.KnowledgeBaseFactory;
+import org.kie.api.KieBaseConfiguration;
+
+import java.util.Properties;
+
+/**
+ * 使用 Properties 为 drools 设置环境参数
+ * @author lkmc2
+ * @date 2019/8/17 10:20
+ */
+public class PropertiesAsConfiguration {
+
+    public static void main(String[] args) {
+        // 设置 drools 的环境参数
+        Properties properties = new Properties();
+        properties.setProperty("org.drools.sequential", "true");
+
+        // 创建配置对象（第二个参数用于指定类加载器，不指定时使用默认的类加载器）
+        KieBaseConfiguration kbConf = KnowledgeBaseFactory.newKnowledgeBaseConfiguration(properties, null);
+
+        // 将环境配置传给知识（knowledge）定义的知识库对象
+        InternalKnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase(kbConf);
+    }
+
+}
