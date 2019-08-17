7388128194ea226587c07b3bfe34d35bf13f27ef
lkmc2 <lkmc2@163.com>
2019-08-17 17:20:34 +0800

    添加无状态的知识会话例子2

--- src/main/java/com/lin/ch01/StatelessKnowledgeSession2.java
+++ src/main/java/com/lin/ch01/StatelessKnowledgeSession2.java
@@ -3,6 +3,7 @@ package com.lin.ch01;
 import org.drools.core.impl.InternalKnowledgeBase;
 import org.drools.core.impl.KnowledgeBaseFactory;
 import org.kie.api.KieBaseConfiguration;
+import org.kie.api.command.Command;
 import org.kie.api.definition.KiePackage;
 import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.StatelessKieSession;
@@ -16,11 +17,11 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * 无状态的知识会话例子1
+ * 无状态的知识会话例子2
  * @author lkmc2
  * @date 2019/8/17 16:14
  */
-public class StatelessKnowledgeSession1 {
+public class StatelessKnowledgeSession2 {
 
     /*
     对 StatelessKnowledgeSession 的介绍：
@@ -35,7 +36,7 @@ public class StatelessKnowledgeSession1 {
     public static void main(String[] args) {
         KnowledgeBuilder kBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         // 添加 drl 资源文件
-        kBuilder.add(ResourceFactory.newClassPathResource("StatelessKnowledgeSession1.drl", StatelessKnowledgeSession1.class),
+        kBuilder.add(ResourceFactory.newClassPathResource("StatelessKnowledgeSession2.drl", StatelessKnowledgeSession2.class),
                      ResourceType.DRL);
 
         // 获取知识包集合（即规则列表）
@@ -53,26 +54,30 @@ public class StatelessKnowledgeSession1 {
         // 创建无状态的知识会话
         StatelessKieSession statelessKieSession = kBase.newStatelessKieSession();
 
-        // 设置要 insert 到 drl 文件的对象（这种对象叫 fact）到列表中
-        List<Object> list = new ArrayList<>();
-        list.add(1);
-        list.add(new Object());
+        // 设置单个全局变量
+//        statelessKieSession.setGlobal("key1", 1);
 
-        // 将 fact 对象列表传给会话，并执行所有规则
-        statelessKieSession.execute(list);
+        // 设置需要批量执行的命令到列表中
+        List<Command> list = new ArrayList<>();
+
+        // 设置需要执行 insert 操作的 fact 对象（fact 对象指的是要传给 drl 文件的 java 对象）
+        list.add(CommandFactory.newInsert(2));
+        list.add(CommandFactory.newInsert(new Object()));
+
+        // 设置全局变量
+        list.add(CommandFactory.newSetGlobal("key1", 1));
+        list.add(CommandFactory.newSetGlobal("key2", new Object()));
+
+        // 批量执行列表中的命令
+        statelessKieSession.execute(CommandFactory.newBatchExecution(list));
 
         /*
         运行结果：
-        I am bigger than 0
+        2 is bigger than 1
         I am a object
+        $obj == key2：false
         I am a object
-         */
-
-        // 如果想把列表当成一个 fact 对象传给 drl 文件，需要使用 CommandFactory.newInsert(obj) 方法来包装
-        statelessKieSession.execute(CommandFactory.newInsert(list));
-        /*
-        运行结果：
-        I am a object
+        $obj == key2：false
          */
     }
 }
